b7de38ced7be8c56fd93c4f35e7a8ebf1209e9d0
Heral <heralk21@students.cs.ubc.ca>
2023-10-11 11:35:24 -0700

    Phase 1 (push 5)

--- src/test/model/ExpenseListTest.java
+++ src/test/model/ExpenseListTest.java
@@ -118,6 +118,137 @@ class ExpenseListTest {
         assertEquals(testexpenselist.returnSavings(),474.24);
     }
 
+    @Test
+    void testRemoveExpenseOnce() {
+        List<Expense> testList = testexpenselist.getAllExpenses();
+        assertTrue(testList.isEmpty());
+        assertEquals(0,testexpenselist.getMonthlyBudget());
+        assertEquals(0,testexpenselist.getTotalSpent());
+        assertEquals(0,testexpenselist.returnSavings());
+
+        assertFalse(testexpenselist.removeExpense("iPad"));
+        assertEquals(0,testexpenselist.getMonthlyBudget());
+        assertEquals(0,testexpenselist.getTotalSpent());
+        assertEquals(0,testexpenselist.returnSavings());
+    }
+
+    @Test
+    void testUpdateBudgetRemoveExpenseThatExistsOnce() {
+        List<Expense> testList = testexpenselist.getAllExpenses();
+        assertTrue(testList.isEmpty());
+        assertEquals(0,testexpenselist.getMonthlyBudget());
+        assertEquals(0,testexpenselist.getTotalSpent());
+        assertEquals(0,testexpenselist.returnSavings());
+
+        assertTrue(testexpenselist.updateBudget(1500.0));
+        assertEquals(1500.0,testexpenselist.getMonthlyBudget());
+
+        assertTrue(testexpenselist.addExpense(expense1));
+        assertEquals(1500.0,testexpenselist.getMonthlyBudget());
+        assertEquals(600.0,testexpenselist.getTotalSpent());
+        assertEquals(900.0,testexpenselist.returnSavings());
+
+
+        assertTrue(testexpenselist.removeExpense("iPad"));
+        assertEquals(1500.0,testexpenselist.getMonthlyBudget());
+        assertEquals(0,testexpenselist.getTotalSpent());
+        assertEquals(1500.0,testexpenselist.returnSavings());
+    }
+
+    @Test
+    void testUpdateBudgetRemoveExpenseThatDoesNotExistOnce() {
+        List<Expense> testList = testexpenselist.getAllExpenses();
+        assertTrue(testList.isEmpty());
+        assertEquals(0,testexpenselist.getMonthlyBudget());
+        assertEquals(0,testexpenselist.getTotalSpent());
+
+        assertTrue(testexpenselist.updateBudget(1500.0));
+
+        assertFalse(testexpenselist.removeExpense("MSP"));
+        assertEquals(1500.0,testexpenselist.getMonthlyBudget());
+        assertEquals(0,testexpenselist.getTotalSpent());
+        assertEquals(1500.0,testexpenselist.returnSavings());
+    }
+
+    @Test
+    void testAddExpenseOnceUpdateBudgetAgain() {
+        List<Expense> testList = testexpenselist.getAllExpenses();
+        assertTrue(testList.isEmpty());
+        assertEquals(testexpenselist.getMonthlyBudget(),0);
+        assertEquals(testexpenselist.getTotalSpent(),0);
+
+        assertTrue(testexpenselist.updateBudget(1500.0));
+
+        assertTrue(testexpenselist.addExpense(expense1));
+        assertEquals(testexpenselist.getMonthlyBudget(),1500.0);
+        assertEquals(testexpenselist.getTotalSpent(),600.0);
+        assertEquals(testexpenselist.returnSavings(),900.0);
+
+        assertTrue(testexpenselist.updateBudget(601.0));
+
+        assertEquals(testexpenselist.getMonthlyBudget(),601.0);
+        assertEquals(testexpenselist.getTotalSpent(),600.0);
+        assertEquals(testexpenselist.returnSavings(),1.0);
+
+    }
+
+    @Test
+    void testAddExpenseOnceUpdateBudgetAgainFail() {
+        List<Expense> testList = testexpenselist.getAllExpenses();
+        assertTrue(testList.isEmpty());
+        assertEquals(testexpenselist.getMonthlyBudget(),0);
+        assertEquals(testexpenselist.getTotalSpent(),0);
+
+        assertTrue(testexpenselist.updateBudget(1500.0));
+
+        assertTrue(testexpenselist.addExpense(expense1));
+        assertEquals(testexpenselist.getMonthlyBudget(),1500.0);
+        assertEquals(testexpenselist.getTotalSpent(),600.0);
+        assertEquals(testexpenselist.returnSavings(),900.0);
+
+        assertFalse(testexpenselist.updateBudget(599.0));
+
+        assertEquals(testexpenselist.getMonthlyBudget(),1500.0);
+        assertEquals(testexpenselist.getTotalSpent(),600.0);
+        assertEquals(testexpenselist.returnSavings(),900.0);
+
+    }
+
+    @Test
+    void testAddMultipleExpenseUpdateBudgetAgain() {
+        List<Expense> testList = testexpenselist.getAllExpenses();
+        assertTrue(testList.isEmpty());
+        assertEquals(testexpenselist.getMonthlyBudget(),0);
+        assertEquals(testexpenselist.getTotalSpent(),0);
+        assertEquals(testexpenselist.returnSavings(),0);
+
+        assertTrue(testexpenselist.updateBudget(1500.0));
+
+        assertTrue(testexpenselist.addExpense(expense1));
+        assertEquals(testexpenselist.getMonthlyBudget(),1500.0);
+        assertEquals(testexpenselist.getTotalSpent(),600.0);
+        assertEquals(testexpenselist.returnSavings(),900.0);
+
+        assertTrue(testexpenselist.addExpense(expense2));
+        assertEquals(testexpenselist.getMonthlyBudget(),1500.0);
+        assertEquals(testexpenselist.getTotalSpent(),1025.76);
+        assertEquals(testexpenselist.returnSavings(),474.24);
+
+        assertTrue(testexpenselist.addExpense(expense3));
+        assertEquals(testexpenselist.getMonthlyBudget(),1500.0);
+        assertEquals(testexpenselist.getTotalSpent(),1112.51);
+        assertEquals(testexpenselist.returnSavings(),387.49);
+
+        assertFalse(testexpenselist.updateBudget(1000.0));
+
+        assertTrue(testexpenselist.updateBudget(1112.6));
+    }
+
+
+
+
+
+
 
 
 
