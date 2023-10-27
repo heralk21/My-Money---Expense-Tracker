6dbea0e6584b1f4a03a9bceb157b3242770d5b42
Heral <heralk21@students.cs.ubc.ca>
2023-10-27 16:48:01 -0700

    final commit - 1.0

--- src/main/persistence/StartApplication.java
+++
@@ -1,9 +0,0 @@
-package persistence;
-
-public class StartApplication {
-    public static void main(String[] args) {
-        String path = "/data/testWriterEmptyExpenseList.json";
-        JsonWriter writer = new JsonWriter(path);
-
-    }
-}
